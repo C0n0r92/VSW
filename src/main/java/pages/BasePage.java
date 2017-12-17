9b7483f997648d872d1e9b39ecae7b9c19543593
conor mc loughlin <conor.mcloughlin@mttnow.com>
2017-12-17 19:33:32 +0000

    Creating base page and imports in build.gradle

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,7 +1,23 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 /**
  * Created by conor on 17/12/17.
  */
 public class BasePage {
-}
+
+        protected final WebDriver driver;
+        public WebDriverWait wait;
+        static final long DEFAULT_EXPLICIT_WAIT = 30;
+
+        public BasePage(WebDriver driver) {
+
+            this.driver = driver;
+            PageFactory.initElements(driver, this);
+            wait = new WebDriverWait(driver, DEFAULT_EXPLICIT_WAIT);
+        }
+    }
+
