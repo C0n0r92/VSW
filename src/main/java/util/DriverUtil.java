aa63d6bd0ff97cd09f775ac1dc16e76dbc019019
conor mc loughlin <conor.mcloughlin@mttnow.com>
2017-12-18 19:00:36 +0000

    deleting old package and renaming typo

--- src/main/java/util/DriverUtil.java
+++ src/main/java/util/DriverUtil.java
@@ -15,7 +15,6 @@ public class DriverUtil {
 
     public static WebDriver getWebDriver(){
 
-
             if (driver == null) {
                 System.setProperty("webdriver.chrome.driver", "/Users/conor/workspaces/VSW/src/drivers/chromedriver");
                 WebDriver d = new ChromeDriver();
